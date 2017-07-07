fff538c16b32c994db8f6c2e5da8997594639707
cexbrayat <cedric@ninja-squad.com>
2017-07-07 09:22:58 +0200

    63fe8f94bf tweak build script

--- @angular/http.js
+++ @angular/http.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-7cf4e7c0a5
+ * @license Angular v4.3.0-beta.1-63fe8f94bf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2088,7 +2088,7 @@ JsonpModule.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-7cf4e7c0a5');
+const VERSION = new Version('4.3.0-beta.1-63fe8f94bf');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-7cf4e7c0a5
+ * @license Angular v4.3.0-beta.1-63fe8f94bf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
