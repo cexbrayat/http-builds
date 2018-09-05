766e04dc1e1ebfc60f42b22095cb43a5b154b870
Misko Hevery <misko@hevery.com>
2018-09-05 17:12:54 +0200

    51c0d9cae9 style(ivy): remove unused ivy code (#25780)

---
+++ esm5/src/version.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Version } from '@angular/core';
+/** *
+ * @deprecated see https://angular.io/guide/http
+  @type {?} */
+export var VERSION = new Version('7.0.0-beta.4-51c0d9cae9');
+//# sourceMappingURL=version.js.map
---
+++ testing.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @license Angular v7.0.0-beta.4-51c0d9cae9
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */ 
+ export * from './testing/testing'
